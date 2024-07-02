57da23dc309a3e3e8730d96b1fb34f48fcfbf854
Javedmv <javedmv777@gmail.com>
2024-07-02 15:50:18 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from './components/NewProject';
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectState] = useState({selectedProject:undefined, projects:[]})
+
+  function handleStartAddProject(){
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null
+      }
+    })
+  }
+
+  function handleAddProject(projectData){
+    setProjectState((prevState) => {
+
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        project:[...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState);
+  let content;
+
+  if(projectsState.selectedProject === null){
+    content = <NewProject onAdd={handleAddProject}/>
+  }else if(projectsState.selectedProject === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,19 @@
+import { useRef } from 'react';
 import Input from "./Input";
 
-function NewProject(){
+function NewProject({onAdd}){
+    const titleRef = useRef();
+    const descriptionRef = useRef();
+    const dueDateRef = useRef();
+
+    function handleSave(){
+        const enteredTitle = titleRef.current.value;
+        const enteredDescriptionRef = descriptionRef.current.value;
+        const enteredDueDateRef = dueDateRef.current.value;
+
+        onAdd({title: enteredTitle, discription: enteredDescriptionRef, dueDate:enteredDueDateRef});
+    }
+
     return(
         <div className="w-[35rem] mt-16">
             <menu className="flex item-center justify-end gap-4 my-4">
@@ -8,13 +21,13 @@ function NewProject(){
                     <button className="px-6 py-2 text-stone-800 hover:text-stone-950">Cancel</button>
                 </li>
                 <li>
-                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>Save</button>
                 </li>
             </menu>
             <div>
-                <Input label='Title' />
-                <Input label='Description' textarea/>
-                <Input label='Due Date' />
+                <Input type='text' ref={titleRef} label='Title' />
+                <Input ref={descriptionRef} label='Description' textarea/>
+                <Input type='date' ref={dueDateRef} label='Due Date' />
             </div>
         </div> 
     )
